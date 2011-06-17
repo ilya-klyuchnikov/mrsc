4387243c5b8a57d699bcf2db5b2a458804396118
Ilya Klyuchnikov <ilya.klyuchnikov@gmail.com>
2011-06-17 16:28:17 +0400

    remove obsolete experiments

--- src/main/scala/mrsc/sll/SLLWhistles.scala
+++ src/main/scala/mrsc/sll/SLLWhistles.scala
@@ -2,44 +2,6 @@ package mrsc.sll
 
 import mrsc._
 
-/*
-case class ExpressionSize(size: Int) extends PartialOrdering[Expr] {
-  val name = "size < " + size
-  def blame[D, E](ps: PState[Expr, D, E]) = ps.node.conf match {
-    case Let(_, _) => None
-    case Ctr(_, _) => None
-    case x if x.size < size => Some(ps.node)
-    case _ => None
-  }
-}
-
-case class MaxGens(max: Int) extends Whistle {
-  val name = "no more than " + max + " lets"
-  def blame[D, E](ps: PState[Expr, D, E]): Option[CoNode[Expr, D, E]] = {
-    val lets = (ps.node :: ps.node.ancestors).count(_.conf.isInstanceOf[Let])
-    if (lets > max)
-      Some(ps.node)
-    else
-      None
-  }
-}
-
-case class MaxLetParts(max: Int) extends Whistle {
-  val name = "no more than " + max + " let parts"
-  def blame[D, E](ps: PState[Expr, D, E]): Option[CoNode[Expr, D, E]] = {
-    val letOpt = (ps.node :: ps.node.ancestors).find(_.conf.isInstanceOf[Let])
-    letOpt match {
-      case Some(CoNode(Let(_, bs), _, _, _, _)) =>
-        if (bs.length > max)
-          Some(ps.node)
-        else None
-      case _ => None
-    }
-
-  }
-}
-*/
-
 object HEWhistle extends SimplePartialOrdering[Expr] {
   val name = "homeomorphic embedding"
   def lteq(e1: Expr, e2: Expr) = e2 match {
@@ -91,18 +53,3 @@ object HEByCouplingWithRedexWhistle extends SimplePartialOrdering[Expr] {
       }
   }
 }
-
-/*
-object Whistles {
-  def or(ws: Whistle*): Whistle =
-    new Whistle {
-      val name = ws map { _.name } mkString ("(", "||", ")")
-      override def blame[D, E](ps: PState[Expr, D, E]): Option[CoNode[Expr, D, E]] = {
-        for (w <- ws) {
-          val r = w.blame(ps)
-          if (r.isDefined) return r
-        }
-        None
-      }
-    }
-}*/
