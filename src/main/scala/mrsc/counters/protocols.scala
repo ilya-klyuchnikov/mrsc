64fad34fb2c2d66f0fc1e4a58c3c51bd067b6bdd
Ilya Klyuchnikov <ilya.klyuchnikov@gmail.com>
2011-07-10 13:17:39 +0400

    more examples with protocols

--- src/main/scala/mrsc/counters/protocols.scala
+++ src/main/scala/mrsc/counters/protocols.scala
@@ -5,23 +5,106 @@ trait Protocol {
   def safe(c: Counter): Boolean
 }
 
-object MESI extends Protocol {
-
-  val rules: List[TransitionRule] = List(
-    {
-      case List(invalid, exclusive, shared, modified) if invalid >= 1 =>
-        List(invalid - 1, 0, shared + exclusive + modified + 1, 0)
+object Synapse extends Protocol {
+  // i = invalid
+  // d = dirty
+  // v = valid
+  val rules: List[TransitionRule] =
+    List({
+      case List(i, d, v) if i >= 1 =>
+        List(i + d - 1, 0, v + 1)
     }, {
-      case List(invalid, exclusive, shared, modified) if exclusive >= 1 =>
-        List(invalid, exclusive - 1, shared, modified + 1)
+      case List(i, d, v) if v >= 1 =>
+        List(i + d + v - 1, 1, 0)
     }, {
-      case List(invalid, exclusive, shared, modified) if shared + invalid >= 1 =>
-        List(invalid + exclusive + shared + modified - 1, 1, 0, 0)
+      case List(i, d, v) if i >= 1 =>
+        List(i + d + v - 1, 1, 0)
     })
 
   def safe(c: Counter) = c match {
-    case List(invalid, exclusive, shared, modified) if modified >= 2 => false
-    case List(invalid, exclusive, shared, modified) if shared >= 1 && modified >= 1 => false
+    case List(i, d, v) if d >= 1 && v >= 1 => false
+    case List(i, d, v) if d >= 2 => false
+    case _ => true
+  }
+}
+
+// invalid, modified, shared
+object MSI extends Protocol {
+  val rules: List[TransitionRule] =
+    List({
+      case List(i, m, s) if i >= 1 =>
+        List(i + m + s - 1, 1, 0)
+    }, {
+      case List(i, m, s) if i >= 1 =>
+        List(i, m, s)
+    }, {
+      case List(i, m, s) if i >= 1 =>
+        List(i - 1, 0, m + s + 1)
+    })
+
+  def safe(c: Counter) = c match {
+    case List(i, m, s) if m >= 1 && s >= 1 => false
+    case List(i, m, s) if m >= 2 => false
+    case _ => true
+  }
+}
+
+// invalid, modified, shared, owned
+object MOSI extends Protocol {
+  val rules: List[TransitionRule] =
+    List({
+      case List(i, o, s, m) if i >= 1 =>
+        List(i - 1, m + o, s + 1, 0)
+    }, {
+      case List(i, o, s, m) if o >= 1 =>
+        List(i + o + s + m - 1, 0, 0, 1)
+    }, {
+      // wI
+      case List(i, o, s, m) if i >= 1 =>
+        List(i + o + s + m - 1, 0, 0, 1)
+    }, {
+      // wS
+      case List(i, o, s, m) if s >= 1 =>
+        List(i + o + s + m - 1, 0, 0, 1)
+    }, {
+      // se
+      case List(i, o, s, m) if s >= 1 =>
+        List(i + 1, o, s - 1, m)
+    }, {
+      // wbm
+      case List(i, o, s, m) if m >= 1 =>
+        List(i + 1, o, s, m - 1)
+    }, {
+      // wbo
+      case List(i, o, s, m) if o >= 1 =>
+        List(i + 1, o - 1, s, m)
+    })
+
+  def safe(c: Counter) = c match {
+    case List(i, o, s, m) if o >= 2 => false
+    case List(i, o, s, m) if m >= 2 => false
+    case List(i, o, s, m) if s >= 1 && m >= 1 => false
+    case _ => true
+  }
+}
+
+object MESI extends Protocol {
+
+  val rules: List[TransitionRule] =
+    List({
+      case List(i, e, s, m) if i >= 1 =>
+        List(i - 1, 0, s + e + m + 1, 0)
+    }, {
+      case List(i, e, s, m) if e >= 1 =>
+        List(i, e - 1, s, m + 1)
+    }, {
+      case List(i, e, s, m) if s + i >= 1 =>
+        List(i + e + s + m - 1, 1, 0, 0)
+    })
+
+  def safe(c: Counter) = c match {
+    case List(i, e, s, m) if m >= 2 => false
+    case List(i, e, s, m) if s >= 1 && m >= 1 => false
     case _ => true
   }
 }
--- src/main/scala/mrsc/counters/samples.scala
+++ src/main/scala/mrsc/counters/samples.scala
@@ -46,10 +46,9 @@ case class CounterMultiSc(val protocol: Protocol, val l: Int)
 
 trait ProtocolSafetyAware extends MagicWhistle {
   val protocol: Protocol
-  override def isDangerous(counter: Counter): Boolean = 
+  override def isDangerous(counter: Counter): Boolean =
     super.isDangerous(counter) || !protocol.safe(counter)
 }
-
 
 object CounterSamples extends App {
   def scProtocol(protocol: Protocol, l: Int, startCounter: Counter): Unit = {
@@ -83,16 +82,15 @@ object CounterSamples extends App {
       println()
     }
   }
-  
+
   def multiScProtocol(protocol: Protocol, l: Int, startCounter: Counter): Unit = {
     val sc = CounterMultiSc(protocol, l)
     val consumer = new SimpleGraphConsumer[Counter, Int]
     //val consumer = new CountGraphConsumer[Counter, DriveInfo[Counter], Extra](100000000)
     val builder = new CoGraphBuilder(sc, consumer)
     builder.buildCoGraph(startCounter, NoExtra)
-    println(consumer.result)
+    //println(consumer.result)
 
-    
     for (graph <- consumer.result) {
       println("================================")
       println()
@@ -106,13 +104,83 @@ object CounterSamples extends App {
   def checkSubTree(safe: Counter => Boolean)(node: Node[Counter, _, _]): Boolean =
     safe(node.conf) && node.outs.map(_.node).forall(checkSubTree(safe))
 
+  def synapse(): Unit = {
+    println()
+    println("Synapse - Direct 1")
+    scProtocol(Synapse, 1, List(ϖ, 0, 0))
+
+    println("Synapse - Direct 2")
+    scProtocol(Synapse, 2, List(ϖ, 0, 0))
+
+    println()
+    println("Synapse - ALL 1")
+    multiScProtocol(Synapse, 1, List(ϖ, 0, 0))
+
+    println()
+    println("Synapse - ALL 2")
+    multiScProtocol(Synapse, 2, List(ϖ, 0, 0))
+  }
+
+  def msi(): Unit = {
+    println()
+    println("MSI - Direct 1")
+    scProtocol(MSI, 1, List(ϖ, 0, 0))
+
+    println()
+    println("MSI - Direct 2")
+    scProtocol(MSI, 2, List(ϖ, 0, 0))
+
+    println()
+    println("MSI - ALL 1")
+    multiScProtocol(MSI, 1, List(ϖ, 0, 0))
+
+    println()
+    println("MSI - ALL 2")
+    multiScProtocol(MSI, 2, List(ϖ, 0, 0))
+  }
+  
+  def mosi(): Unit = {
+    println()
+    println("MOSI - Direct 1")
+    scProtocol(MOSI, 1, List(ϖ, 0, 0, 0))
+
+    println()
+    println("MOSI - Direct 2")
+    scProtocol(MOSI, 2, List(ϖ, 0, 0, 0))
+
+    println()
+    println("MOSI - ALL 1")
+    multiScProtocol(MOSI, 1, List(ϖ, 0, 0, 0))
+
+    println()
+    println("MOSI - ALL 2")
+    multiScProtocol(MOSI, 2, List(ϖ, 0, 0, 0))
+  }
+  
+  def mesi(): Unit = {
+    println()
+    println("MESI - Direct 1")
+    scProtocol(MESI, 1, List(ϖ, 0, 0, 0))
+
+    println()
+    println("MESI - Direct 2")
+    scProtocol(MESI, 2, List(ϖ, 0, 0, 0))
+
+    println()
+    println("MESI - ALL 1")
+    multiScProtocol(MESI, 1, List(ϖ, 0, 0, 0))
+
+    println()
+    println("MESI - ALL 2")
+    multiScProtocol(MESI, 2, List(ϖ, 0, 0, 0))
+  }
+
   //scProtocol(MESI, 0, List(ϖ, 0, 0, 0))
   //scProtocol(MESI, 1, List(ϖ, 0, 0, 0))
-  scProtocol(MESI, 2, List(ϖ, 0, 0, 0))
-
   //simpleMultiScProtocol(MESI, 2, List(ϖ, 0, 0, 0))
-  
-  println("***")
-  multiScProtocol(MESI, 1, List(ϖ, 0, 0, 0))
-  multiScProtocol(MESI, 2, List(ϖ, 0, 0, 0))
+
+  synapse()
+  msi()
+  mosi()
+  mesi()
 }
