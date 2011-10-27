f901f9ba380ff94370f5dc6bc40b5e24e3511183
Ilya Klyuchnikov <ilya.klyuchnikov@gmail.com>
2011-10-27 15:20:54 +0400

    moving code

--- src/main/scala/mrsc/pfp/language.scala
+++ src/main/scala/mrsc/pfp/language.scala
@@ -2,26 +2,6 @@ package mrsc.pfp
 
 import mrsc.core._
 
-/*!# Equivalence and subclass relations on configurations
-  
-  If the current configuration is `equivalent` to another configuration labeling
-  an ancestor node, the supercompiler can loop back to that ancestor.
-  (Looping back to arbitrary completed nodes is also possible if the operation
-  `rollback` is not used, so that non-ancestor nodes cannot be pruned.)
-  If the current configuration is an `instance` of another configuration
-  the supercompiler may perform some actions in order to make looping
-  back possible.
-  
-  At the semantic level a configuration c is regarded as a representation of set(c),
-  a set of states of a computation process.
-  It is assumed that
-    (1) subclass.equiv(c1, c2) implies that set(c1) = set(c2);
-    (2) subclass.lteq(c1, c2) implies set(c1) <= set(c2).
-    
-  When considering configurations as sets, the relation subclass in general is undecidable.
-  However, in PFP setting we consider this relation as being total: syntactic approximation
-  is used usually in traditional supercompilers.
- */
 trait PFPSyntax[C] {
   def subst(c: C, sub: Subst[C]): C
   def findSubst(from: C, to: C): Option[Subst[C]]
@@ -54,5 +34,52 @@ trait MSG[C] extends MutualGens[C] {
   def msg(c1: C, c2: C): Option[RawRebuilding[C]] = {
     val mutual = mutualGens(c1, c2)
     mutual find { rb => mutual forall { other => subclass.lteq(rb._1, other._1) } }
+  }
+}
+
+case class Contraction[C](v: Name, pat: C) {
+  override def toString =
+    if (v != null) v + " = " + pat else ""
+  def subst() = Map[Name, C](v -> pat)
+}
+
+sealed trait DriveInfo[C]
+case class TransientStepInfo[C] extends DriveInfo[C] {
+  override def toString = "->"
+}
+case class DecomposeStepInfo[C](compose: List[C] => C) extends DriveInfo[C] {
+  override def toString = ""
+}
+case class VariantsStepInfo[C](contr: Contraction[C]) extends DriveInfo[C] {
+  override def toString = contr.toString
+}
+
+sealed trait DriveStep[C] {
+  val graphStep: GraphStep[C, DriveInfo[C]]
+}
+
+case class StopDriveStep[C] extends DriveStep[C] {
+  val graphStep =
+    CompleteCurrentNodeStep[C, DriveInfo[C]]()
+}
+
+case class TransientDriveStep[C](next: C) extends DriveStep[C] {
+  val graphStep = {
+    val ns = List((next, TransientStepInfo[C]()))
+    AddChildNodesStep[C, DriveInfo[C]](ns)
+  }
+}
+
+case class DecomposeDriveStep[C](compose: List[C] => C, parts: List[C]) extends DriveStep[C] {
+  val graphStep = {
+    val ns = parts map { (_, DecomposeStepInfo(compose)) }
+    AddChildNodesStep[C, DriveInfo[C]](ns)
+  }
+}
+
+case class VariantsDriveStep[C](cases: List[(C, Contraction[C])]) extends DriveStep[C] {
+  val graphStep = {
+    val ns = cases map { v => (v._1, VariantsStepInfo(v._2)) }
+    AddChildNodesStep[C, DriveInfo[C]](ns)
   }
 }
