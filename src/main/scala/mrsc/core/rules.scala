f76ce806f62e34d860bf30cf7b5505a11033ce40
Ilya Klyuchnikov <ilya.klyuchnikov@gmail.com>
2011-11-19 21:31:44 +0300

    Generalizing framework

---
+++ src/main/scala/mrsc/core/rules.scala
@@ -0,0 +1,16 @@
+package mrsc.core
+
+// The most general form for coding multi-result supercompilation
+trait MRSCRules[C, D] extends GraphRewriteRules[C, D] {
+  type Signal
+  def inspect(g: G): Signal
+
+  def fold(signal: Signal, g: G): List[S]
+  def drive(signal: Signal, g: G): List[S]
+  def rebuild(signal: Signal, g: G): List[S]
+
+  override def steps(g: G): List[S] = {
+    val signal = inspect(g)
+    fold(signal, g) ++ drive(signal, g) ++ rebuild(signal, g)
+  }
+}
