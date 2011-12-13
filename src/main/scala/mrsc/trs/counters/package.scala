0b6b616a76c78f0e64fdfe87edca578b142bde96
Ilya Klyuchnikov <ilya.klyuchnikov@gmail.com>
2011-12-13 17:30:40 +0300

    deleting obsolete (too generic!) code

--- src/main/scala/mrsc/trs/counters/package.scala
+++
@@ -1,8 +0,0 @@
-package mrsc.trs
-
-package object counters {
-  type OmegaConf = List[Component]
-  type TransitionRule = PartialFunction[OmegaConf, OmegaConf]
-  implicit def intToComponent(i: Int): Component = Value(i)
-  val ϖ = Omega
-}
