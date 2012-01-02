a066b1b6e82c8a0c12d5fe893a9936f8cabdd34b
Ilya Klyuchnikov <ilya.klyuchnikov@gmail.com>
2012-01-03 01:08:17 +0300

    some kind of metrics

--- src/main/scala/mrsc/counters/rules.scala
+++ src/main/scala/mrsc/counters/rules.scala
@@ -2,8 +2,46 @@ package mrsc.counters
 
 import mrsc.core._
 
-// There are some optimizations in this code in order to filter out failures
-// and reduce the number of alternatives.
+// No optimizations at all
+case class MRCountersRules0(val protocol: Protocol, l: Int) extends GraphRewriteRules[Conf, Int] {
+  var maxSize: Int = Int.MaxValue
+  def inspect(g: G) = g.current.conf exists {
+    case Num(i) => i >= l
+    case Omega  => false
+  }
+
+  def fold(g: G): Option[S] =
+    g.completeNodes.find { n => Conf.instanceOf(g.current.conf, n.conf) } map { n => FoldStep(n.sPath): S }
+
+  def drive(dangerous: Boolean, g: G): List[S] =
+    if (dangerous) {
+      List()
+    } else {
+      val subSteps = for ((next, i) <- next(g.current.conf).zipWithIndex if next.isDefined) yield (next.get, i + 1)
+      if (subSteps.isEmpty) {
+        List(CompleteCurrentNodeStep())
+      } else {
+        List(AddChildNodesStep(subSteps))
+      }
+    }
+
+  def next(c: Conf) = protocol.rules.map { _.lift(c) }
+
+  def rebuild(dangerous: Boolean, g: G): List[S] =
+    Conf.rebuildings(g.current.conf) map { RebuildStep(_): S }
+
+  override def steps(g: G): List[S] =
+    fold(g) match {
+      case Some(s) => List(s)
+      case None =>
+        val signal = inspect(g)
+        rebuild(signal, g) ++ drive(signal, g)
+    }
+
+  private def size(g: G) =
+    g.completeNodes.size + g.incompleteLeaves.size
+}
+
 case class MRCountersRules(val protocol: Protocol, l: Int) extends GraphRewriteRules[Conf, Int] {
   var maxSize: Int = Int.MaxValue
   def inspect(g: G) = g.current.conf exists {
@@ -45,50 +83,10 @@ case class MRCountersRules(val protocol: Protocol, l: Int) extends GraphRewriteR
         case Some(s) => List(s)
         case None =>
           val signal = inspect(g)
-           rebuild(signal, g) ++ drive(signal, g)
+          rebuild(signal, g) ++ drive(signal, g)
       }
     }
-  
+
   private def size(g: G) =
     g.completeNodes.size + g.incompleteLeaves.size
-}
-
-// A (not elegant so far) version of single-result supercompiler for counters.
-case class SRCountersRules(val protocol: Protocol, l: Int) extends GraphRewriteRules[Conf, Int] {
-  def inspect(g: G) = g.current.conf exists {
-    case Num(i) => i >= l
-    case Omega  => false
-  }
-
-  def fold(g: G): Option[S] =
-    g.completeNodes.find { n => Conf.instanceOf(g.current.conf, n.conf) } map { n => FoldStep(n.sPath): S }
-
-  def drive(dangerous: Boolean, g: G): List[S] =
-    if (dangerous) {
-      List()
-    } else {
-      val subSteps = for ((next, i) <- next(g.current.conf).zipWithIndex if next.isDefined) yield (next.get, i + 1)
-      if (subSteps.isEmpty) {
-        List(CompleteCurrentNodeStep())
-      } else {
-        List(AddChildNodesStep(subSteps))
-      }
-    }
-
-  def next(c: Conf) = protocol.rules.map { _.lift(c) }
-
-  def rebuild(dangerous: Boolean, g: G): List[S] =
-    if (dangerous) {
-      List(RebuildStep(g.current.conf.map { e => if (e >= l) Omega else e }))
-    } else {
-      List()
-    }
-
-  override def steps(g: G): List[S] =
-    fold(g) match {
-      case Some(s) => List(s)
-      case None =>
-        val signal = inspect(g)
-        drive(signal, g) ++ rebuild(signal, g)
-    }
 }
