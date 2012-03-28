6f4715cc0523e6ea289a92867eb7707008292cba
Ilya Klyuchnikov <ilya.klyuchnikov@gmail.com>
2012-03-29 00:41:11 +0400

    working deforestation

--- src/test/scala/mrsc/test/GraphOperationsSpec.scala
+++ src/test/scala/mrsc/test/GraphOperationsSpec.scala
@@ -1,15 +1,9 @@
 package mrsc.test
 
-import org.specs2._
-import org.junit.runner.RunWith
-import org.specs2.runner.JUnitRunner
-
+import org.scalatest.Spec
 import mrsc.core._
 
-@RunWith(classOf[JUnitRunner])
-class GraphOperationsSpec extends mutable.Specification {
-
-  args(sequential = true)
+class GraphOperationsSpec extends Spec {
 
   var g1, g2: SGraph[Int, String] = _
   var tg1, tg2: TGraph[Int, String] = _
@@ -26,105 +20,106 @@ class GraphOperationsSpec extends mutable.Specification {
     SGraph(List(startNode), Nil, Nil)
   }
 
-  "The builder of the 1st graph" in {
-    "starts with a single-node graph" in {
+  describe("The builder of the 1st graph") {
+    it("starts with a single-node graph") {
       g1 = start(0)
 
-      (g1.current.conf must_== 0) and
-        (g1.completeNodes.size must_== 0) and
-        (g1.incompleteLeaves.size must_== 1)
+      assert(g1.current.conf == 0)
+      assert(g1.completeNodes.size === 0)
+      (g1.incompleteLeaves.size === 1)
     }
 
-    "executes AddChildNodesStep" in {
+    it("executes AddChildNodesStep #1") {
       g1 = GraphGenerator.executeStep(AddChildNodesStep(List((1, "0 -> 1"), (2, "0 -> 2"))), g1)
 
-      (g1.current.conf must_== 1) and
-        (g1.completeNodes.size must_== 1) and
-        (g1.incompleteLeaves.size must_== 2)
+      assert(g1.current.conf === 1)
+      assert(g1.completeNodes.size === 1)
+      assert(g1.incompleteLeaves.size === 2)
     }
 
-    "executes CompleteCurrentNodeStep" in {
+    it("executes CompleteCurrentNodeStep") {
       g1 = GraphGenerator.executeStep(CompleteCurrentNodeStep(), g1)
 
-      (g1.current.conf must_== 2) and
-        (g1.completeNodes.size must_== 2) and
-        (g1.incompleteLeaves.size must_== 1)
+      assert(g1.current.conf === 2)
+      assert(g1.completeNodes.size === 2)
+      assert(g1.incompleteLeaves.size === 1)
     }
 
-    "executes RebuildStep" in {
+    it("executes RebuildStep") {
       val oldActive = g1.current
       g1 = GraphGenerator.executeStep(RebuildStep(21), g1)
       val newActive = g1.current
 
-      (newActive.conf must_== 21) and
-        (newActive.in must_== oldActive.in) and
-        (g1.completeNodes.size must_== 2) and
-        (g1.incompleteLeaves.size must_== 1)
+      assert(newActive.conf === 21)
+      assert(newActive.in === oldActive.in)
+      assert(g1.completeNodes.size === 2)
+      assert(g1.incompleteLeaves.size === 1)
     }
 
-    "executes RollbackStep" in {
+    it("executes RollbackStep") {
       val root = g1.current.in.node
       g1 = GraphGenerator.executeStep(RollbackStep(root.sPath, -1), g1)
 
-      (g1.current.conf must_== -1) and
-        (g1.completeNodes.size must_== 0) and
-        (g1.incompleteLeaves.size must_== 1)
+      assert(g1.current.conf === -1)
+      assert(g1.completeNodes.size === 0)
+      assert(g1.incompleteLeaves.size === 1)
     }
 
-    "executes AddChildNodesStep" in {
+    it("executes AddChildNodesStep #2") {
       g1 = GraphGenerator.executeStep(AddChildNodesStep(List((11, "-1 -> 11"))), g1)
 
-      (g1.current.conf must_== 11) and
-        (g1.completeNodes.size must_== 1) and
-        (g1.incompleteLeaves.size must_== 1)
+      assert(g1.current.conf === 11)
+      assert(g1.completeNodes.size === 1)
+      assert(g1.incompleteLeaves.size === 1)
     }
 
-    "executes FoldStep" in {
+    it("executes FoldStep") {
       val root = g1.current.in.node
       g1 = GraphGenerator.executeStep(FoldStep(root.sPath), g1)
 
-      (g1.current must_== null) and
-        (g1.completeNodes.size must_== 2) and
-        (g1.incompleteLeaves.size must_== 0)
+      assert(g1.current === null)
+      assert(g1.completeNodes.size === 2)
+      assert(g1.incompleteLeaves.size === 0)
     }
   }
 
-  "The builder of the 2nd graph" in {
-    "starts with a single-node graph" in {
+  describe("The builder of the 2nd graph") {
+    it("starts with a single-node graph") {
       g2 = start(-1)
 
-      (g2.current.conf must_== -1) and
-        (g2.completeNodes.size must_== 0) and
-        (g2.incompleteLeaves.size must_== 1)
+      assert(g2.current.conf === -1)
+      assert(g2.completeNodes.size === 0)
+      assert(g2.incompleteLeaves.size === 1)
     }
 
-    "executes AddChildNodesStep" in {
+    it("executes AddChildNodesStep") {
       g2 = GraphGenerator.executeStep(AddChildNodesStep(List((11, "-1 -> 11"))), g2)
 
-      (g2.current.conf must_== 11) and
-        (g2.completeNodes.size must_== 1) and
-        (g2.incompleteLeaves.size must_== 1)
+      assert(g2.current.conf === 11)
+      assert(g2.completeNodes.size === 1)
+      assert(g2.incompleteLeaves.size === 1)
     }
 
-    "executes FoldStep" in {
+    it("executes FoldStep") {
       val root = g2.current.in.node
       g2 = GraphGenerator.executeStep(FoldStep(root.sPath), g2)
 
-      (g1.current must_== null) and
-        (g1.completeNodes.size must_== 2) and
-        (g1.incompleteLeaves.size must_== 0)
+      assert(g1.current === null)
+      assert(g1.completeNodes.size === 2)
+      assert(g1.incompleteLeaves.size === 0)
     }
   }
 
-  "Produced graphs" should {
-    "be equal to each other" in {
-      g1 must_== g2
-    }
-    "be equal to sample graph" in {
+  describe("Produced graphs") {
+    //"be equal to each other" in {
+    //  g1 === g2
+    //}
+    it("be equal to sample graph") {
       val tgraph1 = Transformations.transpose(g1)
       val tgraph2 = Transformations.transpose(g2)
 
-      (tgraph1 must_== graph) and (tgraph2 must_== graph)
+      assert(tgraph1 === graph)
+      assert(tgraph2 === graph)
     }
   }
 }
--- src/test/scala/mrsc/test/GraphSpec.scala
+++ src/test/scala/mrsc/test/GraphSpec.scala
@@ -1,30 +1,25 @@
 package mrsc.test
 
-import org.specs2._
-import org.junit.runner.RunWith
-import org.specs2.runner.JUnitRunner
+import org.scalatest.Spec
 
 import mrsc.core._
 
-object GraphSpec extends mutable.Specification {
+object GraphSpec {
 
   def mkGraph(root: SNode[Int, Int] = null,
-      nodes: List[SNode[Int, Int]] = List(),
-      leaves: List[SNode[Int, Int]] = List()) = {
+    nodes: List[SNode[Int, Int]] = List(),
+    leaves: List[SNode[Int, Int]] = List()) = {
     SGraph(List(), leaves, nodes)
-  }  
+  }
 }
 
-@RunWith(classOf[JUnitRunner])
-class GraphSpec extends mutable.Specification {
+class GraphSpec extends Spec {
   import mrsc.test.GraphSpec.mkGraph
-  
-  args(sequential = true)
 
   var graph1, graph1a, graph2, graph2a: SGraph[Int, Int] = _
   var tgraph1, tgraph2: TGraph[Int, Int] = _
 
-  "SNodes and SEdges are created from the top down" in {
+  describe("SNodes and SEdges are created from the top down") {
 
     val cn0 = SNode[Int, Int](conf = 0, in = null, base = None, sPath = List())
     val ce1 = SEdge[Int, Int](cn0, 1)
@@ -36,42 +31,38 @@ class GraphSpec extends mutable.Specification {
     val ce4 = SEdge[Int, Int](cn1, 4)
     val cn4 = SNode[Int, Int](conf = 4, in = ce4, base = Some(List()), sPath = List(1, 0))
 
-    "the top subtrees can be reused for constructing different SGraphs" in {
+    it("the top subtrees can be reused for constructing different SGraphs") {
       graph1 = mkGraph(root = cn0, nodes = List(cn0, cn1, cn2), leaves = List(cn1, cn2))
       graph2 = mkGraph(root = cn0, nodes = List(cn0, cn1, cn2, cn3, cn4), leaves = List(cn2, cn3, cn4))
-      success
+
     }
 
-    "nodes and leaves can be put into SGrpaph in unnatural order" in {
+    it("nodes and leaves can be put into SGrpaph in unnatural order") {
       graph1a = mkGraph(root = cn0, nodes = List(cn0, cn2, cn1), leaves = List(cn2, cn1))
       graph2a = mkGraph(root = cn0, nodes = List(cn0, cn1, cn2, cn3, cn4).reverse, leaves = List(cn2, cn3, cn4).reverse)
-      success
     }
 
   }
 
-  "TNodes and TEdges are created from the bottom up" in {
+  describe("TNodes and TEdges are created from the bottom up") {
     val n2 = TNode[Int, Int](conf = 2, outs = List(), base = None, tPath = List(1))
     val n1 = TNode[Int, Int](conf = 1, outs = List(), base = None, tPath = List(0))
     val e2 = TEdge[Int, Int](n2, 2)
     val e1 = TEdge[Int, Int](n1, 1)
     val n0 = TNode[Int, Int](conf = 0, outs = List(e1, e2), base = None, tPath = List())
 
-    "TGraph is contructed by enumerating root and leaves" in {
+    it("TGraph is contructed by enumerating root and leaves") {
       tgraph1 = TGraph(root = n0, leaves = List(n1, n2))
-      success
     }
 
-    "TNodes can be queried by its path" in ({
-      tgraph1.get(List()) must_== n0
-    } and {
-      tgraph1.get(List(0)) must_== n1
-    } and {
-      tgraph1.get(List(1)) must_== n2
-    })
+    it("TNodes can be queried by its path") {
+      assert(tgraph1.get(List()) === n0)
+      assert(tgraph1.get(List(0)) === n1)
+      assert(tgraph1.get(List(1)) === n2)
+    }
   }
 
-  "Top subtrees cannnot be reused in different TGraphs, so extra top subtrees are created" in {
+  describe("Top subtrees cannnot be reused in different TGraphs, so extra top subtrees are created") {
     val n4 = TNode[Int, Int](conf = 4, outs = List(), base = Some(List()), tPath = List(0, 1))
     val n3 = TNode[Int, Int](conf = 3, outs = List(), base = Some(List(0, 1)), tPath = List(0, 0))
     val e3 = TEdge[Int, Int](n3, 3)
@@ -83,35 +74,28 @@ class GraphSpec extends mutable.Specification {
     val n0 = TNode[Int, Int](conf = 0, outs = List(e1, e2), base = None, tPath = List())
     TGraph(root = n0, leaves = List(n2, n3, n4))
 
-    "TGraph is contructed by enumerating root and leaves" in {
+    it("TGraph is contructed by enumerating root and leaves") {
       tgraph2 = TGraph(root = n0, leaves = List(n2, n3, n4))
-      success
     }
 
-    "TNodes can be queried by its path" in ({
-      tgraph2.get(List()) must_== n0
-    } and {
-      tgraph2.get(List(0)) must_== n1
-    } and {
-      tgraph2.get(List(1)) must_== n2
-    } and {
-      tgraph2.get(List(0, 0)) must_== n3
-    } and {
-      tgraph2.get(List(0, 1)) must_== n4
-    })
+    it("TNodes can be queried by its path") {
+      assert(tgraph2.get(List()) === n0)
+      assert(tgraph2.get(List(0)) === n1)
+      assert(tgraph2.get(List(1)) === n2)
+      assert(tgraph2.get(List(0, 0)) === n3)
+      assert(tgraph2.get(List(0, 1)) === n4)
+    }
   }
 
-  "Transformations" should {
-    "transpose SGraphs into corresponding TGraphs" in ({
-      Transformations.transpose(graph1) must_== tgraph1
-    } and {
-      Transformations.transpose(graph2) must_== tgraph2
-    })
-    
-    "and canonize corresponding graphs: the order of nodes, edges and leave should be natural" in ({
-      Transformations.transpose(graph1a) must_== tgraph1
-    } and {
-      Transformations.transpose(graph2a) must_== tgraph2
-    })
+  describe("Transformations") {
+    it("transpose SGraphs into corresponding TGraphs") {
+      assert(Transformations.transpose(graph1) === tgraph1)
+      assert(Transformations.transpose(graph2) === tgraph2)
+    }
+
+    it("and canonize corresponding graphs: the order of nodes, edges and leave should be natural") {
+      assert(Transformations.transpose(graph1a) === tgraph1)
+      assert(Transformations.transpose(graph2a) === tgraph2)
+    }
   }
 }
