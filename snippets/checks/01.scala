64d1eec1504bd350af5fbb871ff64d47b8e87b7d
Ilya Klyuchnikov <ilya.klyuchnikov@gmail.com>
2012-08-14 23:25:50 +0400

    some experimental stuff

--- snippets/checks/01.scala
+++ snippets/checks/01.scala
@@ -1,8 +1,8 @@
 import mrsc.pfp.experiments._
 import mrsc.pfp.test._
 
-SimpleChecker.run("pfp/nat02.pfp", multi.allDepthBound1(10))
+SimpleChecker.run("pfp/nats/02.pfp", multi.allDepthBound1(10))
 
-SimpleChecker.run("pfp/nat02.pfp", multi.allDepthBound1(20))
+SimpleChecker.run("pfp/nats/02.pfp", multi.allDepthBound1(20))
 
-SimpleChecker.run("pfp/nat03.pfp", multi.allDepthBound1(20))
+SimpleChecker.run("pfp/nats/03.pfp", multi.allDepthBound1(20))
