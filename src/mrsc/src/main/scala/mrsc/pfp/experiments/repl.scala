b0fb434678cc7ef75dbeb3362b91d65a45c8118e
Ilya Klyuchnikov <ilya.klyuchnikov@gmail.com>
2013-03-24 01:50:12 +0400

    one more example refactored/documented

--- src/mrsc/src/main/scala/mrsc/pfp/experiments/repl.scala
+++ src/mrsc/src/main/scala/mrsc/pfp/experiments/repl.scala
@@ -5,6 +5,10 @@ import mrsc.pfp._
 
 object repl {
 
+  import scalaz._
+  import Scalaz._
+  import NamelessShows._
+
   def graphs(file: String, sc: PFPSC): Iterator[SGraph[MetaTerm, Label]] = {
     val (_, task) = io.taskFromFile(file)
     val rules = sc(task.bindings)
@@ -23,9 +27,9 @@ object repl {
       sGraph.completeLeaves.foreach {
         case leaf@SNode(_, _, Some(ePath), _) =>
           val Some(embedded) = leaf.ancestors.find(_.sPath == ePath)
-          Console.println(embedded.conf)
+          Console.println(embedded.conf.shows)
           Console.println("<*>")
-          Console.println(leaf.conf)
+          Console.println(leaf.conf.shows)
           Console.println()
         case _ =>
       }
@@ -34,6 +38,6 @@ object repl {
   def showResiduals(file: String, sc: PFPSC) =
     graphs(file, sc).zipWithIndex.foreach { case (sGraph, i) =>
       val tGraph = Transformations.transpose(sGraph)
-      Console.println(Residuator(tGraph).result)
+      Console.println(Residuator(tGraph).result.shows)
     }
 }
---
+++ src/samples/src/main/scala/PFP05Repl.scala
@@ -0,0 +1,41 @@
+import mrsc.pfp._
+import mrsc.pfp.experiments._
+import samples.SC2
+
+// TODO: refactor to several examples
+// TODO: refactor graph pretty-printing and serialization printing to use Show
+// Right now this sample demonstrate several facilities:
+// 1. It is easy to define "semi-supercompilers" that performs very simple analysis.
+//    E.g.: EmbeddingsDetectors.SC2 performs folding when embedding is detected.
+// 2. Facilities of Simple REPL (that runs supercompiler and outputs something from graph).
+//    In this example it outputs expressions for which whistle blows.
+// 3. An attempt to convert O(n^2) function into O(n) function
+//    (see pfp/s001/fin.pfp). The whistle blows for two expressions
+//    that cannot be folded. After supercompilation both of them they
+//    can be folded. Moreover: this step introduces super-linear speedup.
+object PFP05Repl extends scala.App {
+  import scalaz._
+  import Scalaz._
+  import NamelessShows._
+
+  Console.println("## Supercompiling by EmbeddingsDetectors.SC2")
+  Console.println("## Graph")
+  repl.showGraphs("pfp/s001/fin2.pfp", EmbeddingsDetectors.SC2)
+  Console.println()
+
+  Console.println("## Embeddings")
+  repl.showFoldings("pfp/s001/fin2.pfp", EmbeddingsDetectors.SC2)
+  Console.println()
+
+  Console.println("## Supercompiling the pair of expressions")
+
+  Console.println("### the top expression")
+  val (_, t1) = io.taskFromFile("pfp/s001/fin2a.pfp")
+  Console.println(t1.goal.shows)
+  repl.showResiduals("pfp/s001/fin2a.pfp", SC2)
+
+  Console.println("### renaming of the bottom expression")
+  val (_, t2) = io.taskFromFile("pfp/s001/fin2b.pfp")
+  Console.println(t2.goal.shows)
+  repl.showResiduals("pfp/s001/fin2b.pfp", SC2)
+}
