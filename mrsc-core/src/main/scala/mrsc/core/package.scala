d627aed75090e5ed5014484210b99900ccff64b6
Ilya Klyuchnikov <ilya.klyuchnikov@gmail.com>
2021-10-23 10:52:20 +0100

    code cleanup

--- mrsc-core/src/main/scala/mrsc/core/package.scala
+++ mrsc-core/src/main/scala/mrsc/core/package.scala
@@ -3,9 +3,4 @@ package mrsc
 package object core {
   type TPath = List[Int]
   type SPath = List[Int]
-
-  /** The following exception usually means that some modeling expectation (or hypothesis)
-    * was not met during actual modeling.
-    */
-  class ModelingError(val message: String) extends Exception(message: String)
 }
